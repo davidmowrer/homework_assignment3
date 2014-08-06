fd7ccfc9f0e39d8b017ef479ca16af34f0705c1d
David Mowrer <david.mowrer@comcast.net>
2014-08-06 10:49:05 -0600

    Method_and_recursion/bottles of beer on the wall

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -5,18 +5,15 @@ bottles = 99
 while bottles != 1
 
   puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer."
-  puts
   bottles -= 1
-  puts "Take one down and pass it around, #{bottles} bottles of beer on the wall"
+  puts "Take one down and pass it around, #{bottles} bottles of beer on the wall."
   puts
 
   if bottles == 1
     puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer."
-    puts
-    puts 'Take one down and pass it around, no more bottles of beer on the wall'
+    puts 'Take one down and pass it around, no more bottles of beer on the wall.'
     puts
     puts 'No more bottles of beer on the wall, no more bottles of beer.'
-    puts
     puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
   end
 
